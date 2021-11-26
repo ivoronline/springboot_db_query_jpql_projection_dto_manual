2678221acce9bdc7d135ca0a08dbc2a2efbb158a
ivoronline <ivoronline@gmail.com>
2021-11-26 20:02:16 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_db_query_jpql_projection_dto_manual/repositories/PersonRepository.java
+++ src/main/java/com/ivoronline/springboot_db_query_jpql_projection_dto_manual/repositories/PersonRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PersonRepository extends CrudRepository<Person, Integer> {
-  @Query(nativeQuery = true, value = "SELECT name, age FROM PERSON WHERE NAME = 'John'")
+  @Query(value = "SELECT person.name, person.age FROM Person person WHERE person.name = 'John'")
   Object selectPerson();
 }
